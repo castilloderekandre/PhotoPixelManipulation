25db0e612448ade2c74b1875e7bb012a8b2ea5e5
castilloderekandre <castilloderekandre@gmail.com>
2025-05-21 21:28:35 -0600

    Add ffmpeg wasm module for image proc

--- image-ascii-backend/image-functions/asciiGenerator.ts
+++ image-ascii-backend/image-functions/asciiGenerator.ts
@@ -1,4 +1,5 @@
 import bilateralfilter from "./bilateralFilter";
+import * as ffmpeg from "@ffmpeg/core";
 
 /**
  * The AsciiGenerator class manages the functionality for converting an image into ASCii.
@@ -13,6 +14,7 @@ class AsciiGenerator {
 
     constructor() {
         this.imageResult = "";
+        ffmpeg()
     }
 
     /** The core entrypoint for creating an image.
