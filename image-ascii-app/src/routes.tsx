299593ad9fc460c2bb7502e6ee4db539c271f436
Derek Castillo <castilloderekandre@gmail.com>
2025-07-26 22:38:16 -0500

    Added appropiate logic for Router foundation and Navbar

--- image-ascii-app/src/routes.tsx
+++ image-ascii-app/src/routes.tsx
@@ -1,3 +1,39 @@
 import type { ReactElement } from "react";
+import HomePage from "./pages/Home/HomePage";
+import ProjectsPage from "./pages/Projects/ProjectsPage";
+import ContactPage from "./pages/Contact/ContactPage";
+import NotFoundPage from "./pages/Not Found/NotFoundPage";
 
+interface AppRoute {
+	path: string;
+	label?: string;
+	element: ReactElement;
+	showInNav?: boolean;
+}
 
+export const appRoutes: AppRoute[] = [
+	{
+		path: '/home',
+		label: 'Home',
+		element: <HomePage />,
+		showInNav: true,
+	},
+	{
+		path: '/projects',
+		label: 'Projects',
+		element: <ProjectsPage />,
+		showInNav: true,
+	},
+	{
+		path: '/contact',
+		label: 'Contact',
+		element: <ContactPage />,
+		showInNav: true,
+	},
+	{
+		path: '/notfound',
+		element: <NotFoundPage />,
+	},
+]
+
+export const navLinks = appRoutes.filter(route => route.label && route.showInNav).map(route => ({ path: route.path, label: route.label! }));
